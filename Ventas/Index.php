19d3a708ee4b37ccd53b8cb30c347d2ab248c4e0
PamelaVillalba <139662519+PamelaVillalba@users.noreply.github.com>
2023-08-25 22:43:30 -0400

    alerta si desea eliminar

--- Ventas/Index.php
+++ Ventas/Index.php
@@ -4,20 +4,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CRUD Productos</title>
+    <title>Ventas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    
-    <link rel="stylesheet" href="/CRUD_MYSQLI-productos/estilos/estilos.css">
-
+    <link rel="stylesheet" href="/CRUD_MYSQLI-categoria/estilos/estilos.css">
 </head>
 
-<body style="
+<body style=" 
   width: 100%;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #68bb7e;">
+  background-color: #68bb7e;" >
 
     <!--Círculo-->
     <div class="circulo"></div>
@@ -25,56 +23,60 @@
 
     <!--Header-->
     <header>
-    <a href="/l_page/inicio.php" class="logo">CAN </a>
+    <a href="/l_page/inicio.php" class="logo">CAN</a>
     </header>
     <!--Fin Header-->
 
-<!--CRUD-->
+    <!--CRUD-->
 <div class="container bg-dark p-3 border border-dark rounded position-absolute">
-    <h1 class="bg-secondary p-2 text-white text-center rounded">SISTEMA CAN - CRUD Productos</h1>
 
+    <h1 class="bg-secondary p-2 text-white text-center rounded">SISTEMA CAN - Ventas</h1>
     <br>
     <div class="container">
-        <a href="Forms/AgregarProducto.php" class="btn bg-light border border-dark">Agregar producto</a>
-        <a href="reportes.php" class="btn bg-light border border-dark">Imprimir Reporte</a>
+        <a href="Forms/AgregarCategoria.php" class="btn bg-light border border-dark">Agregar Ventas</a>
+
     </div>
     <br>
     <div class="container bg-light p-3 border border-dark rounded">
-        <h1>Productos</h1>
+        <h1>Ventas</h1>
         <table class="table">
             <thead class="table-dark">
                 <tr>
                     <th scope="col">ID</th>
-                    <th scope="col">DESCRIPCIÓN</th>
-                    <th scope="col">PROVEEDOR</th>
-                    <th scope="col">PRECIO</th>
-                    <th scope="col">STOCK</th>
-                    <th scope="col">IMAGEN</th>
+                    <th scope="col">FECHA CREACION</th>
+                    <th scope="col">CODIGO</th>
+                    <th scope="col">CLIENTE</th>
+                    <th scope="col">PRODUCTO</th>
+                    <th scope="col">MONTO</th>
+                    <th scope="col">CODIGO</th>
+
+
                     <th scope="col"></th>
+
+
+
+
+
+
                 </tr>
             </thead>
             <tbody>
 
             <?php
             include ("Config/Conexion.php");
-            $sql = 'SELECT * FROM producto';
+            $sql = 'SELECT * FROM categoria';
             $query = mysqli_query($link, $sql);
             
             while ($fila = mysqli_fetch_array($query)){
                 ?>
                 <tr>
-                    <th scope="row"><?php echo $fila['idprod']?></th>
-                    <th scope="row"><?php echo $fila['descripcion']?></th>
-                    <th scope="row"><?php echo $fila['proveedor']?></th>
-                    <th scope="row"><?php echo $fila['precio']?></th>
-                    <th scope="row"><?php echo $fila['stock']?></th>
+                    <th scope="row"><?php echo $fila['id']?></th>
+                    <th scope="row"><?php echo $fila['nombre']?></th>
                     <th scope="row"><?php echo $fila['imagen']?></th>
-                    
                     <th scope="row">
                         <!--php?ID se conecta con editardatophp en $_REQUEST['ID']; $fila['id'] se conecta con la columna id de la db sys_can-->
-                        <a href="Forms/EditarDato.php?ID=<?php echo $fila['idprod']?>" class="btn text-white text-center bg-secondary">Editar Datos</a>
-                        <a href="CRUD/EliminarDato.php?ID=<?php echo $fila['idprod']?>" onclick="return  confirm ('ESTA SEGURO'); false" class="btn btn-danger">Eliminar Datos</a>
-
+                        <a href="Forms/EditarDato.php?ID=<?php echo $fila['id']?>" class="btn text-white text-center bg-secondary">Editar Datos</a>
+                        <a href="CRUD/EliminarDato.php?ID=<?php echo $fila['id']?>" class="btn btn-danger">Eliminar Datos</a>
                     </th>
                 </tr>
                 <?php
@@ -86,7 +88,7 @@
     <br>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     </div>
-            <!--Fin de CRUD-->
+        <!--Fin de CRUD-->
 
     <!--Footer-->
     <footer>
